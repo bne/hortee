01724d4d80e4e8b61f54f78398a79c6d570e3179
ben <ben@yard.(none)>
2010-06-13 22:55:09 +0100

    Event model and tests

--- src/tracktor/models.py
+++ src/tracktor/models.py
@@ -1,8 +1,36 @@
+from datetime import datetime
+
 from django.db import models
 
 class Actor(models.Model):
     """An item to which things happen
     """
     name = models.CharField(max_length=100)
-    start = models.DateTimeField(auto_now_add=True)
-    end = models.DateTimeField(blank=True, null=True)
+    
+    @property
+    def events(self):
+        return Event.objects.filter(actor=self)
+        
+    @property
+    def date_start(self):
+        return Event.objects.order_by('date')[0].date
+        
+    @property
+    def date_end(self):
+        return Event.objects.order_by('-date')[0].date
+        
+class Event(models.Model):
+    """Base class for all events on the timeline
+    """
+    actor = models.ForeignKey(Actor)
+    name = models.CharField(max_length=100)
+    date = models.DateTimeField(default=datetime.now)
+    
+    class Meta:
+        ordering = ["date"]
+    
+
+
+
+
+
--- src/tracktor/tests.py
+++ src/tracktor/tests.py
@@ -2,19 +2,64 @@ import time
 from datetime import datetime
 
 from django.test import TestCase
-from tracktor.models import Actor
+from tracktor.models import Actor, Event
 
 class EnvTest(TestCase):
     def testEnv(self):
         self.assert_(True)
         
 class ActorTest(TestCase):
+    def testCreate(self):
+        self.actor = Actor.objects.create(name="test_actor")
+        self.assertEquals(self.actor.name, "test_actor")
+        
+class EventTest(TestCase):
     def setUp(self):
         self.actor = Actor.objects.create(name="test_actor")
         
     def testCreate(self):
-        self.assertEquals(self.actor.name, "test_actor")
+        self.event = Event.objects.create(actor=self.actor, name="test_event")
+        self.assertEquals(self.event.name, 'test_event')
         self.assertAlmostEqual(
-            time.mktime(self.actor.start.timetuple()), 
+            time.mktime(self.event.date.timetuple()), 
             time.mktime(datetime.now().timetuple()))
-        self.assertEquals(self.actor.end, None)
+            
+class TimelineTest(TestCase):
+    def setUp(self):
+        self.actor = Actor.objects.create(name="test_actor")
+        Event.objects.create(actor=self.actor, name="test_event-1",
+            date=datetime(2000, 2, 2, 2, 2, 2))
+        Event.objects.create(actor=self.actor, name="test_event-2",
+            date=datetime(2000, 1, 1, 1, 1, 1))
+        Event.objects.create(actor=self.actor, name="test-event-3",
+            date=datetime(2000, 5, 5, 5, 5, 5))
+        Event.objects.create(actor=self.actor, name="test-event-4",
+            date=datetime(2000, 4, 4, 4, 4, 4))        
+        Event.objects.create(actor=self.actor, name="test-event-5",
+            date=datetime(2000, 3, 3, 3, 3, 3))
+        
+    def testCreate(self):
+        self.assertEquals(len(self.actor.events), 5)
+        
+    def testStart(self):
+        self.assertEquals(self.actor.date_start, datetime(2000, 1, 1, 1, 1, 1))
+        
+    def testEnd(self):
+        self.assertEquals(self.actor.date_end, datetime(2000, 5, 5, 5, 5, 5))
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
