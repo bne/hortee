52a0eba6874e2e1cd3f69b15725ae47b4a11ba0c
bne <ben@hyl.co.uk>
2010-06-14 00:25:04 +0100

    guh, gotta go to bed

--- src/hortee/tracktor/tests.py
+++ src/hortee/tracktor/tests.py
@@ -2,7 +2,7 @@ import time
 from datetime import datetime
 
 from django.test import TestCase
-from tracktor.models import Actor, Event
+from hortee.tracktor.models import Actor, Event
 
 class EnvTest(TestCase):
     def testEnv(self):
