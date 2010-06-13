eb8661dcc43324439c39eb5909dae26d12a079f6
bne <ben@hyl.co.uk>
2010-06-13 23:43:29 +0100

    Switched name

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "django-tracktor",
+    name = "hortee",
     version = "0.1",
-    url = "http://github.com/bne/django-tracktor",
+    url = "http://github.com/bne/hortee",
     license = "Apache2",
-    description = "Event driven django timeline thing",
+    description = "Event driven django plant tracking thing",
     long_description = read('README'),
     author = "Ben Miller",
     author_email = "ben@hyl.co.uk",
