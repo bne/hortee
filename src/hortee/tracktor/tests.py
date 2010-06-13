6cd3db07ccfe9a23bd87b2244ac60213048b9659
bne <ben@hyl.co.uk>
2010-06-14 00:21:07 +0100

    Adjusted tests again

--- src/hortee/tracktor/tests.py
+++ src/hortee/tracktor/tests.py
@@ -2,7 +2,7 @@ import time
 from datetime import datetime
 
 from django.test import TestCase
-from hortee.tracktor.models import Actor, Event
+from tracktor.models import Actor, Event
 
 class EnvTest(TestCase):
     def testEnv(self):
