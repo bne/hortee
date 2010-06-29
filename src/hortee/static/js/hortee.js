358da821697fe967ba6a7998cb780f5dbdb194b4
bne <ben@hyl.co.uk>
2010-06-29 23:53:27 +0100

    Added processing.js

---
+++ src/hortee/static/js/hortee.js
@@ -0,0 +1,17 @@
+$(function(){
+    var canvas = $('canvas').get(0);
+    var p = Processing(canvas);
+    p.size(300, 300);
+    p.stroke(255);
+    
+    function draw(p) {
+        p.background(0);
+        y = y - 1;
+        if (y < 0) y = p.height;
+        p.line(0, y, p.width, y);
+    }
+    var y = 100;
+    var frameRate = 90;
+    var interval = 1000.0 / frameRate;
+    setInterval(function () { draw(p); }, interval);
+});
