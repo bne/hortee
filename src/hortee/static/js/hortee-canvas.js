7a30db59c2c31e0e7fa279a57faa1f414006d966
bne <ben@hyl.co.uk>
2010-07-01 23:23:09 +0100

    draw rectangle

--- src/hortee/static/js/hortee-canvas.js
+++ src/hortee/static/js/hortee-canvas.js
@@ -5,15 +5,28 @@ $(function(){
     p.stroke(255);
     
     function draw(p) {
-        p.background(0);
-        y = y - 1;
-        if (y < 0) y = p.height;
-        p.line(0, y, p.width, y);
+        p.rect(
+            start_pos.x, start_pos.y, 
+            end_pos.x - start_pos.x, end_pos.y - start_pos.y);
     }
-    var y = 100;
-    var frameRate = 90;
-    var interval = 1000.0 / frameRate;
+        
+    var drag_start = false;
+    var start_pos = { x:0, y:0 };
+    var end_pos = { x:0, y:0 };
     
-    draw(p);
-    //setInterval(function () { draw(p); }, interval);
+    $(document).mousedown(function(ev){
+        drag_start = true;
+        start_pos.x = ev.pageX;
+        start_pos.y = ev.pageY;
+    });
+    
+    $(document).mousemove(function(ev){
+        end_pos.x = ev.pageX;
+        end_pos.y = ev.pageY;
+    });
+    
+    $(document).mouseup(function(ev){
+        drag_start = false;
+        draw(p);
+    });
 });
