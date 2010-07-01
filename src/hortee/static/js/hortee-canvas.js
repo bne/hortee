c3d9a66558c40f140ba58d390fef1b4bb2991c42
bne <ben@hyl.co.uk>
2010-07-02 00:28:50 +0100

    loads shapes from array

--- src/hortee/static/js/hortee-canvas.js
+++ src/hortee/static/js/hortee-canvas.js
@@ -2,13 +2,11 @@ $(function(){
     var canvas = $('canvas').get(0);
     var p = Processing(canvas);
     p.size($(document).width(), $(document).height());
-    p.stroke(255);
+    p.stroke(0);
     
-    function draw(p) {
-        p.rect(
-            start_pos.x, start_pos.y, 
-            end_pos.x - start_pos.x, end_pos.y - start_pos.y);
-    }
+    var cur_sh = 'rect';
+    var sh = [];
+
         
     var drag_start = false;
     var start_pos = { x:0, y:0 };
@@ -21,12 +19,25 @@ $(function(){
     });
     
     $(document).mousemove(function(ev){
-        end_pos.x = ev.pageX;
-        end_pos.y = ev.pageY;
+        if(drag_start){
+            end_pos.x = ev.pageX;
+            end_pos.y = ev.pageY;
+            p[cur_sh].apply(this, [start_pos.x, start_pos.y, 
+                end_pos.x - start_pos.x, end_pos.y - start_pos.y]);            
+            draw(p);
+        }
     });
     
     $(document).mouseup(function(ev){
         drag_start = false;
-        draw(p);
-    });
+        sh.push([cur_sh, [start_pos.x, start_pos.y, 
+            end_pos.x - start_pos.x, end_pos.y - start_pos.y]]);
+    });    
+    
+    function draw(p) {
+        for(var i=0;i<sh.length;i++){
+            p.noFill();
+            p[sh[i][0]].apply(this, sh[i][1]);
+        }
+    }
 });
