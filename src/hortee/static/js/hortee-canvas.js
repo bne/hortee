3eed83085a52cda47b16b4f3784f0da95344da3b
bne <ben@hyl.co.uk>
2010-07-03 01:11:29 +0100

    Resize

--- src/hortee/static/js/hortee-canvas.js
+++ src/hortee/static/js/hortee-canvas.js
@@ -1,23 +1,23 @@
 $(function(){
     var canvas = $('canvas');
-    canvas.attr('height', $(document).height());
-    canvas.attr('width', $(document).width());
-    
     var p = Processing(canvas.get(0));
-    
-    
+    var cur_sh = 'rect';
+    var sh = [];     
     var bg_col = '#FFFFFF';
     var fg_col = '#000000';
-    var ac_col = '#FF0000';
-    
-
-    
-    var cur_sh = 'rect';
-    var sh = [];
-        
+    var ac_col = '#FF0000';     
     var drag_start = false;
     var start_pos = { x:0, y:0 };
     var end_pos = { x:0, y:0 };
+
+    function resize_canvas(){    
+        canvas.attr('height', $(document).height());
+        canvas.attr('width', $('#header').width());
+        p = Processing(canvas.get(0));
+        draw();
+    }
+    $(window).resize(resize_canvas);
+    resize_canvas();
     
     $(document).mousedown(function(ev){
         drag_start = true;
@@ -39,11 +39,9 @@ $(function(){
     
     $(document).mouseup(function(ev){
         if(drag_start){
-            if(start_pos !== end_pos) {
-                sh.push([cur_sh, [start_pos.x, start_pos.y, 
-                    end_pos.x - start_pos.x, end_pos.y - start_pos.y]]);
-                draw();
-            }
+            sh.push([cur_sh, [start_pos.x, start_pos.y, 
+                end_pos.x - start_pos.x, end_pos.y - start_pos.y]]);
+            draw();
             drag_start = false;
         }
     });    
