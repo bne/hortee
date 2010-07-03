d0eba476c7622a19fdcea02b8aa5ed5613f150d6
bne <ben@hyl.co.uk>
2010-07-04 00:11:45 +0100

    Removed excess array elements and fill from first sprite

--- src/hortee/static/js/hortee-canvas.js
+++ src/hortee/static/js/hortee-canvas.js
@@ -1,8 +1,8 @@
 $(function(){
     var canvas = $('canvas');
     var p = Processing(canvas.get(0));
-    var cur_sh = 'rect';
-    var sh = [];     
+    var cur_shape = 'rect';
+    var sprites = [];     
     var bg_col = '#FFFFFF';
     var fg_col = '#000000';
     var ac_col = '#FF0000';     
@@ -12,12 +12,16 @@ $(function(){
 
     function resize_canvas(){    
         canvas.attr('height', $(document).height());
-        canvas.attr('width', $('#header').width());
+        canvas.attr('width', $(document).width());
         p = Processing(canvas.get(0));
         draw();
     }
     $(window).resize(resize_canvas);
     resize_canvas();
+    
+    function serialise(){
+        return sprites.join(',');
+    }
     
     $(document).mousedown(function(ev){
         drag_start = true;
@@ -29,28 +33,36 @@ $(function(){
         if(drag_start){        
             p.background(bg_col);
             p.stroke(ac_col);
+            p.noFill();
             end_pos.x = ev.pageX;
             end_pos.y = ev.pageY;
-            p[cur_sh].apply(this, [start_pos.x, start_pos.y, 
-                end_pos.x - start_pos.x, end_pos.y - start_pos.y]);
+            p[cur_shape].apply(this, [start_pos.x, start_pos.y, 
+              end_pos.x - start_pos.x, end_pos.y - start_pos.y]);
             draw();           
         }
     });
     
     $(document).mouseup(function(ev){
         if(drag_start){
-            sh.push([cur_sh, [start_pos.x, start_pos.y, 
-                end_pos.x - start_pos.x, end_pos.y - start_pos.y]]);
+            sprite = [cur_shape, [start_pos.x, start_pos.y, 
+              end_pos.x - start_pos.x, end_pos.y - start_pos.y]];
+                        
+            if(!sprites.length || 
+              (sprites.length && 
+              (sprite.join() != sprites[sprites.length-1].join()))){
+                sprites.push(sprite);
+            }
             draw();
             drag_start = false;
         }
+        console.log(serialise());
     });    
     
     function draw() {
         p.stroke(fg_col);
-        for(var i=0;i<sh.length;i++){
+        for(var i=0;i<sprites.length;i++){
             p.noFill();
-            p[sh[i][0]].apply(this, sh[i][1]);
+            p[sprites[i][0]].apply(this, sprites[i][1]);
         }
     }
 });
