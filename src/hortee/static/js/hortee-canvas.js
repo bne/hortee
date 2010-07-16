38afbadc8f083713862b65f0dd38c3075fce4830
Ben Miller <ben.miller@isotoma.com>
2010-07-16 17:07:56 +0100

    Removed dependency on Processing

--- src/hortee/static/js/hortee-canvas.js
+++ src/hortee/static/js/hortee-canvas.js
@@ -1,56 +1,43 @@
 $(function(){
     var canvas = $('canvas');
-    var p = Processing(canvas.get(0));
-    var cur_sh = 'rect';
-    var sh = [];     
-    var bg_col = '#FFFFFF';
-    var fg_col = '#000000';
+    var ctx = canvas.get(0).getContext('2d');  
+
+    var bg_col = '#FFFF00';
+    var fg_col = '#00FF00';
     var ac_col = '#FF0000';     
     var drag_start = false;
-    var start_pos = { x:0, y:0 };
+    var start_pos = { x:0, y:0 };    
     var end_pos = { x:0, y:0 };
 
-    function resize_canvas(){    
-        canvas.attr('height', $(document).height());
-        canvas.attr('width', $('#header').width());
-        p = Processing(canvas.get(0));
-        draw();
-    }
-    $(window).resize(resize_canvas);
-    resize_canvas();
+    canvas.attr('height', $(document).height());
+    canvas.attr('width', $(document).width());
     
     $(document).mousedown(function(ev){
+        ctx.save();
         drag_start = true;
         start_pos.x = end_pos.x = ev.pageX;
         start_pos.y = end_pos.y = ev.pageY;
     });
     
     $(document).mousemove(function(ev){
-        if(drag_start){        
-            p.background(bg_col);
-            p.stroke(ac_col);
-            end_pos.x = ev.pageX;
-            end_pos.y = ev.pageY;
-            p[cur_sh].apply(this, [start_pos.x, start_pos.y, 
-                end_pos.x - start_pos.x, end_pos.y - start_pos.y]);
-            draw();           
+        if(drag_start){
+            ctx.clearRect(0, 0, canvas.width(), canvas.height());
+            ctx.strokeStyle = ac_col;
+            ctx.strokeRect(
+                start_pos.x, start_pos.y, 
+                ev.pageX - start_pos.x, ev.pageY - start_pos.y);
+            ctx.restore();
         }
     });
     
     $(document).mouseup(function(ev){
         if(drag_start){
-            sh.push([cur_sh, [start_pos.x, start_pos.y, 
-                end_pos.x - start_pos.x, end_pos.y - start_pos.y]]);
-            draw();
+            ctx.strokeStyle = fg_col;
+            ctx.strokeRect(
+                start_pos.x, start_pos.y, 
+                ev.pageX - start_pos.x, ev.pageY - start_pos.y);
+
             drag_start = false;
         }
-    });    
-    
-    function draw() {
-        p.stroke(fg_col);
-        for(var i=0;i<sh.length;i++){
-            p.noFill();
-            p[sh[i][0]].apply(this, sh[i][1]);
-        }
-    }
+    }); 
 });
