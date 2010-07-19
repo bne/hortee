087b278e27abc78e571b591441e0139870319256
Ben Miller <ben.miller@isotoma.com>
2010-07-19 18:42:33 +0100

    Caches sprites

--- src/hortee/static/js/hortee-canvas.js
+++ src/hortee/static/js/hortee-canvas.js
@@ -4,40 +4,57 @@ $(function(){
 
     var bg_col = '#FFFF00';
     var fg_col = '#00FF00';
-    var ac_col = '#FF0000';     
+    var ac_col = '#FF0000';    
+     
     var drag_start = false;
-    var start_pos = { x:0, y:0 };    
-    var end_pos = { x:0, y:0 };
-
+    var start_pos;
+    var sprites = [];
+    
     canvas.attr('height', $(document).height());
     canvas.attr('width', $(document).width());
     
     $(document).mousedown(function(ev){
-        ctx.save();
         drag_start = true;
-        start_pos.x = end_pos.x = ev.pageX;
-        start_pos.y = end_pos.y = ev.pageY;
+        start_pos = { 'x': ev.pageX, 'y': ev.pageY };
     });
     
     $(document).mousemove(function(ev){
         if(drag_start){
             ctx.clearRect(0, 0, canvas.width(), canvas.height());
-            ctx.strokeStyle = ac_col;
-            ctx.strokeRect(
-                start_pos.x, start_pos.y, 
+            ctx.beginPath();
+            ctx.rect(start_pos.x, start_pos.y, 
                 ev.pageX - start_pos.x, ev.pageY - start_pos.y);
-            ctx.restore();
+            ctx.strokeStyle = ac_col;
+            ctx.stroke();
+            draw();
         }
+        
+        //console.log(ctx.isPointInPath(ev.pageX, ev.pageY));        
     });
     
     $(document).mouseup(function(ev){
         if(drag_start){
-            ctx.strokeStyle = fg_col;
-            ctx.strokeRect(
-                start_pos.x, start_pos.y, 
-                ev.pageX - start_pos.x, ev.pageY - start_pos.y);
-
+            
+            sprites.push({
+                'path': [
+                    {'shape': 'rect', 'args': [start_pos.x, start_pos.y, ev.pageX - start_pos.x, ev.pageY - start_pos.y]}
+                ]
+            });
+            
             drag_start = false;
+            draw();
         }
-    }); 
+    });
+    
+    function draw() {        
+        for(var i=0, sprite; sprite=sprites[i]; i++) {
+            ctx.beginPath();
+            ctx.strokeStyle = fg_col;
+            for(var j=0, shape; shape=sprite.path[j]; j++) {
+                ctx[shape.shape].apply(ctx, shape.args);
+            }
+            ctx.stroke();
+            ctx.closePath();
+        }        
+    }
 });
