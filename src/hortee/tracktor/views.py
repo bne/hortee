cd23ec8714a34b8406f13ace7381126a8ca14ddc
bne <ben@hyl.co.uk>
2010-07-30 23:07:43 +0100

    List actors

--- src/hortee/tracktor/views.py
+++ src/hortee/tracktor/views.py
@@ -4,7 +4,7 @@ from django.template import RequestContext
 from models import Plot, Actor
 
 def list_actors(request):
-    actors = None
+    actors = Actor.objects.filter(plot__owners=request.user)
     return render_to_response('list.html', {
         'actors': actors,
         }, context_instance=RequestContext(request))
