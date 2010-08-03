81933bdcd2f890a685d43b3d97ee7fed9a9d0808
bne <ben@hyl.co.uk>
2010-08-03 22:19:46 +0100

    Slidey events

--- src/hortee/static/js/hortee-list.js
+++ src/hortee/static/js/hortee-list.js
@@ -1,7 +1,14 @@
 $(function(){
     $('#actors li').click(function(){
-        $.get('/list/'+this.id, function(data){
-            
-        });
+        var events = $(this).find('ul.events');
+        if(!events.html()) {
+            $.get('/list/'+this.id, function(data){
+                events.append(data);
+                events.slideDown();
+            });
+        }
+        else {
+            events.slideToggle();
+        }
     });
 });
