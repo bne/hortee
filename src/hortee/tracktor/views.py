f8f3b807d774f70f49a2327bc030c88de865c0f4
bne <ben@hyl.co.uk>
2010-09-15 23:34:00 +0100

    Comments for views

--- src/hortee/tracktor/views.py
+++ src/hortee/tracktor/views.py
@@ -8,11 +8,15 @@ from models import Plot, Actor, Event
 
 @login_required
 def _map(request):
+    """View for plot map
+    """
     return render_to_response('map.html', {
         }, context_instance=RequestContext(request))
         
 @login_required
 def _list(request):
+    """View for actor list
+    """
     actors = Actor.objects.filter(plot__owners=request.user).order_by('-id')
     return render_to_response('list.html', {
             'actors': actors,
@@ -20,6 +24,8 @@ def _list(request):
         
 @login_required
 def add_actor(request):
+    """View for adding an actor to a plot
+    """
     actors = []
     if request.method == 'POST':
         name = request.POST.get('name', None)
@@ -34,6 +40,8 @@ def add_actor(request):
 
 @login_required
 def delete_actor(request):
+    """View for deleting an actor from a plot
+    """
     id = None
     if request.method == 'POST':
         try:
@@ -44,7 +52,18 @@ def delete_actor(request):
     return HttpResponse(id) 
 
 @login_required
+def list_events(request, id=None):
+    """View for listing events
+    """
+    events = Event.objects.filter(actor=id)
+    return render_to_response('list-events.html', {
+            'events': events,
+        }, context_instance=RequestContext(request))
+        
+@login_required
 def add_event(request):
+    """View for adding an event to an actor
+    """
     events = []
     if request.method == 'POST':
         actor_id = request.POST.get('actor_id', None)
@@ -60,6 +79,8 @@ def add_event(request):
 
 @login_required
 def delete_event(request):
+    """View for deleting an event
+    """
     id = None
     if request.method == 'POST':
         try:
@@ -69,10 +90,4 @@ def delete_event(request):
             pass
     return HttpResponse(id) 
 
-@login_required
-def list_events(request, id=None):
-    events = Event.objects.filter(actor=id)
-    return render_to_response('list-events.html', {
-            'events': events,
-        }, context_instance=RequestContext(request))
     
