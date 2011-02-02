ae0d6800937d98b579dc12d58f75af92d2e86980
bne <ben@hyl.co.uk>
2011-02-02 21:29:19 +0000

    Killing everything

--- src/hortee/main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class UserProfile(models.Model):
-    """Additional user information
-    """
-    user = models.OneToOneField(User)
-    settings = models.TextField(null=True, blank=True)
-    
--- src/hortee/tracktor/context_processors.py
+++
@@ -1,9 +0,0 @@
-from models import Plot
-
-def tracktor(request):
-    rtn = {}
-    if request.user.is_authenticated():
-        rtn['plots'] = Plot.objects.filter(owners=request.user)
-    return rtn
-    
-    
