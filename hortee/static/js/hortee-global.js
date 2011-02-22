1398fc0857dae34fcdebde295c0ab785874ffad7
bne <ben@hyl.co.uk>
2011-02-22 00:18:25 +0000

    messages object

--- hortee/static/js/hortee-global.js
+++ hortee/static/js/hortee-global.js
@@ -1,6 +1,30 @@
+var hortee = {
+    messages: {
+        hide_timeout: 5000,
+        add: function(msg, tag) {
+            var tag = tag || 'info';            
+            idx = $('ul.messages').append($('<li class="'+ tag +'">'+ msg +'</li>')).find('li').length-1;
+            hortee.messages.hide(idx);
+        },
+        hide: function(idx) {
+            window.setTimeout(function(){ 
+                $($('ul.messages li').get(idx)).hide();
+            }, hortee.messages.hide_timeout);
+        },
+        debug: function(msg)   { return hortee.messages.add(msg, 'debug'); },
+        info: function(msg)    { return hortee.messages.add(msg, 'info'); },
+        success: function(msg) { return hortee.messages.add(msg, 'success'); },
+        warning: function(msg) { return hortee.messages.add(msg, 'warning'); },
+        error: function(msg)   { return hortee.messages.add(msg, 'error'); }
+    }
+};
+
+
 $(function() {
+    // Mustache style templates for underscore
     _.templateSettings = {
         evaluate    : /{%([\s\S]+?)%}/g,
         interpolate : /{{([\s\S]+?)}}/g
     };
 });
+
