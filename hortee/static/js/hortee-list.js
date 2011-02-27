7eb1701fda55b748852c8623284a3a4da7bfa582
bne <ben@hyl.co.uk>
2011-02-27 23:16:10 +0000

    Wired in events list

--- hortee/static/js/hortee-list.js
+++ hortee/static/js/hortee-list.js
@@ -12,16 +12,39 @@ $(function(){
             return data.objects;
         }        
     });
-
+    
+    window.EventView = Backbone.View.extend({
+        tagName: 'li',
+        className: 'event',
+        template: _.template($('#event-template').html()),
+        initialize: function() {
+            this.model.view = this;
+        },
+        render: function() {
+            $(this.el).html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+ 
     window.Actor = Backbone.Model.extend({
         initialize: function() {
+            _.bindAll(this, 'addOne', 'addAll', 'render');
             this.events = new window.Events;
             this.events.url = API_DISCO['event'].list_endpoint + '?actor=' + this.id;
             this.events.actor = this;
+            this.events.bind('add', this.addOne);
+            this.events.bind('refresh', this.addAll);
         },
         url: function() {
             return this.get('resource_uri');
-        }        
+        },        
+        addAll: function(){
+          this.events.each(this.addOne);
+        },
+        addOne: function(event){
+          var view = new EventView({ model: event });
+          $(this.view.el).find('ul.events').append(view.render().el);
+        },
     });
     
     window.Actors = Backbone.Collection.extend({
@@ -37,8 +60,7 @@ $(function(){
         className: 'actor',
         template: _.template($('#actor-template').html()),
         events: {
-            //'tap h3 .event-add': 'toggleEventAdd',
-            'click h3 .event-add': 'toggleEventAdd'
+            'click h3': 'toggleEvents'
         },
         initialize: function() {
             this.model.view = this;
@@ -47,38 +69,37 @@ $(function(){
             $(this.el).html(this.template(this.model.toJSON()));
             return this;
         },
-        toggleEventAdd: function(ev) {
-            ev.cancelBubble = true;
-            var form = $(this.el).find('form.event');
-            if(form.css('display') == 'none') {
-                form.show();
-            }
-            else {
-                form.hide();
+        toggleEvents: function(ev) {
+            var event_list = $(this.el).find('ul.events');
+            var show = (event_list.css('display') == 'none');
+            $('ul.events').hide();
+            if(show) {
+                event_list.show();
+                if(!this.model.events.loaded) {
+                    this.model.events.fetch();
+                    this.model.events.loaded = true;
+                }
             }
         }
     });
     
-    window.App = Backbone.View.extend({
+    window.AppView = Backbone.View.extend({
         el: $('#app'),
         initialize: function() {
             _.bindAll(this, 'addOne', 'addAll', 'render');
             this.actors = new Actors();
             this.actors.bind('add', this.addOne);
             this.actors.bind('refresh', this.addAll);
-            this.actors.bind('all', this.render);
             this.actors.fetch();
-        },
-        
+        },        
         addAll: function(){
           this.actors.each(this.addOne);
         },
-
         addOne: function(actor){
           var view = new ActorView({ model: actor });
-          this.$('#actors').append(view.render().el);
+          $('#actors').append(view.render().el);
         },
     });    
 
-    window.app = new App();  
+    window.app = new AppView();  
 });
